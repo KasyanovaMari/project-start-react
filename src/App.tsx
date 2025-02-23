5ddb22a3b1d2b0dd4f91264e9ec39a659eb3e5c4
KasyanovaMari <magivara0@gmail.com>
2025-02-23 18:56:18 +0600

    second commit 0 homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {log} from "node:util";
 
-const App =() => {
+const App = () => {
+    console.log('App rendered')
     return (
-    <div className="App">
-        <ul>
-            <li>css</li>
-            <li>html</li>
-        </ul>
-    </div>
-  );
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
 }
 
+function AppTitle() {
+    return <>This is APP component</>
+}
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+function Star() {
+    console.log('Star rendered')
+    return (
+        <div>star</div>
+    )
+}
+
+function Accordion() {
+    console.log('Accordion rendered')
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    );
+}
+
+function AccordionTitle() {
+    console.log('AccordionTitle rendering')
+    return (
+        <h3>Menu</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
 export default App;
